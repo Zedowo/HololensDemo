2841abaf6ca909dbcaefb0d255ff5e73d14d72a1
Evan Lin <linevan6@gmail.com>
2023-04-19 14:03:34 -0400

    Added RadialView script onto TextChange

--- Assets/Scripts/TestTextChange.cs
+++ Assets/Scripts/TestTextChange.cs
@@ -2,52 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Unity.Profiling;
+using Microsoft.MixedReality.Toolkit.SpatialManipulation;
 
 public class TestTextChange : MonoBehaviour
-{
-    public TextMeshPro textMesH;
-    public GameObject allCube;
-    public GameObject mainSlate;
-    public GameObject rotationCube;
-
-    //public RadialView radialView;
-
-    Vector3 originalPosition = new Vector3(0.124f, 1.905f, 2.096f);
-    Vector3 leftPosition = new Vector3(-1.071f, 1.905f, 2.096f);
-    Vector3 rightCubePosition = new Vector3(0.934f, 1.464f, 2.138f);
-    Vector3 restingCubePosition = new Vector3(1f, -10f, -10f);
-
-
-    void Awake ()
     {
-        //find the textMeshPro object
-        textMesH = GetComponent<TextMeshPro>();
-        //radialView = allCube.FindObjectOfType<RadialView>();
-    }
+        public TextMeshPro textMesH;
+        public GameObject allCube;
+        public GameObject mainSlate;
+        public GameObject rotationCube;
 
-    public void printer()
-    {
-        Debug.Log("Hello World");
-    }
- 
-    public void updateText(int counter)
-    {
-        //each case is the next iteration for the next part of the tutorial. Strings combined downwards for readability.
-        switch (counter)
+        public RadialView radialView;
+
+        Vector3 originalPosition = new Vector3(0.124f, 1.905f, 2.096f);
+        Vector3 leftPosition = new Vector3(-1.071f, 1.905f, 2.096f);
+        Vector3 rightCubePosition = new Vector3(0.934f, 1.464f, 2.138f);
+        Vector3 restingCubePosition = new Vector3(1f, -10f, -10f);
+
+
+        private void Awake()
         {
-            case 0:
-                textMesH.text = "These three objects are scripts that typically work in tangent with one another. When you add ObjectManipulator to an object, Constraints are automatically added (as this is the default).\r\n\r\nTo properly move these objects," +
-                "however, you also need a specific script named UGUIInputAdapterDraggable (you can find this by simply looking this up under \"Assets\" and all.) This is what allows the object to be interacted with.\r\n";
-                break;
+            //find the textMeshPro object
+            textMesH = GetComponent<TextMeshPro>();
+            radialView = allCube.GetComponent<RadialView>();
+        }
 
-            case 1:
-                textMesH.text = "First, let's take a look at a default interaction with all of these componenets -- no constraints added. With this box, you can move it around, change its size, and change its rotation. " +
-                "The number of hands you use also matters when using these componenets. You'll notice that you aren't able to change an object's size with just one hand.";
-                break;
+        public void printer()
+        {
+            Debug.Log("Hello World");
+        }
+ 
+        public void updateText(int counter)
+        {
+            //each case is the next iteration for the next part of the tutorial. Strings combined downwards for readability.
+            switch (counter)
+            {
+                case 0:
+                    textMesH.text = "These three objects are scripts that typically work in tangent with one another. When you add ObjectManipulator to an object, Constraints are automatically added (as this is the default).\r\n\r\nTo properly move these objects," +
+                    "however, you also need a specific script named UGUIInputAdapterDraggable (you can find this by simply looking this up under \"Assets\" and all.) This is what allows the object to be interacted with.\r\n";
+                    radialView.enabled = false;
+                    break;
 
-            /*case 2:
-                textMesH.text = ""*/
+                case 1:
+                    textMesH.text = "First, let's take a look at a default interaction with all of these componenets -- no constraints added. With this box, you can move it around, change its size, and change its rotation. " +
+                    "The number of hands you use also matters when using these componenets. You'll notice that you aren't able to change an object's size with just one hand.";
+                    radialView.enabled = true;
+                    break;
+
+                /*case 2:
+                    textMesH.text = ""*/
          
+            }
         }
     }
-}
