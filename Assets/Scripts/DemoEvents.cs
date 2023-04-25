59c37f128ceb2a8945c0a735201829f07420bd37
Evan Lin <linevan6@gmail.com>
2023-04-25 14:05:40 -0400

    Added prefabs for all cube constraints, and added fundementals to the basic scene

--- Assets/Scripts/DemoEvents.cs
+++ Assets/Scripts/DemoEvents.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DemoEvents : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public TextMeshPro textMesH;
+    public GameObject allCube;
+    public GameObject mainSlate;
+
+    void Awake()
     {
-        
+        textMesH = mainSlate.GetComponent<TextMeshPro>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void updateText(int counter)
     {
-        
+        //each case is the next iteration for the next part of the tutorial. Strings combined downwards for readability.
+        switch (counter)
+        {
+            case (0):
+                textMesH.text = "The Hololens 2 has many options for user interaction. While there is the standard \"walk up and interact\", there are a myraid of options that allow for ease of use." +
+                        "\r\n\r\nSome of these include:\r\n\r\n     " +
+                        "- Gaze and Pinch\r\n     - Eye Tracking\r\n     - Speech Recognition\r\n";
+                break;
+
+            case (1):
+                textMesH.text = "Try to input";
+                break;
+
+        }
     }
+
 }
