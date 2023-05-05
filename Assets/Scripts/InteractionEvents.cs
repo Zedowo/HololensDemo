8c50ef417098a32c658034900e23b1eb6c4a1115
Evan Lin <linevan6@gmail.com>
2023-05-05 14:05:35 -0400

    Finished Interactions Scene

--- Assets/Scripts/InteractionEvents.cs
+++ Assets/Scripts/InteractionEvents.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Microsoft.MixedReality.Toolkit.Utilities.Solvers;
 
 public class InteractionEvents : MonoBehaviour
 {
@@ -18,6 +19,10 @@ public class InteractionEvents : MonoBehaviour
     public GameObject rayCube;
     public GameObject holdCube;
 
+    public Follow followMeRay;
+    public Follow followMeGaze;
+    public Follow followMeHold;
+
     Color white = new Color(255f, 255f, 255f, 0.8f);
     Color red = new Color(255f, 0f, 40f, 0.8f);
     Color blue = new Color(0f, 66f, 255f, 0.8f);
@@ -33,6 +38,9 @@ public class InteractionEvents : MonoBehaviour
         cubeRendererGaze = gazeCube.GetComponent<Renderer>();
         cubeRendererRay = rayCube.GetComponent<Renderer>();
         cubeRendererHold = holdCube.GetComponent<Renderer>();
+        followMeRay = rayCube.GetComponent<Follow>();
+        followMeHold = holdCube.GetComponent<Follow>();
+        followMeGaze = gazeCube.GetComponent<Follow>();
     }
 
     public void resetColorGaze()
@@ -65,6 +73,42 @@ public class InteractionEvents : MonoBehaviour
         cubeRendererHold.material.SetColor("_Color", blue);
     }
 
+    public void followTrigger()
+    {
+        switch (caseScenerio)
+        {
+            case 1:
+                followMeHold.enabled = true;
+                break;
+
+            case 2:
+                followMeRay.enabled = true;
+                break;
+
+            case 3:
+                followMeGaze.enabled = true;
+                break;
+        }
+    }
+
+    public void deFollow()
+    {
+        switch (caseScenerio)
+        {
+            case 1:
+                followMeHold.enabled = false;
+                break;
+
+            case 2:
+                followMeRay.enabled = false;
+                break;
+
+            case 3:
+                followMeGaze.enabled = false;
+                break;
+        }
+    }
+
     /*public IEnumerator changeColors()
     {
         while (true)
@@ -84,6 +128,31 @@ public class InteractionEvents : MonoBehaviour
             case 0:
                 textMesH.text = "MRTK is also able to detect certain interactions types from the user. The examples that will be demonstrated will be following:\r\n\r\n     - Eye Tracking\r\n" +
                     "     - Ray Pointing\r\n     - Hold Detection";
+                followMeHold.enabled = false;
+                holdCube.transform.position = objectStorage;
+                break;
+            case 1:
+                textMesH.text = "First, Hold Interactions. When grabbing the object, you'll notice the cube will turn a different color. This \"event\" will stop whenever your hands are off the cube.";
+                followMeRay.enabled = false;
+                holdCube.transform.position = centerCubePosition;
+                rayCube.transform.position = objectStorage;
+                break;
+
+            case 2:
+                textMesH.text = "Next, we'll explore the ray interaction. Point your finger towards the cube and align the ray line with box. ";
+                followMeHold.enabled = false;
+                followMeGaze.enabled = false;
+                holdCube.transform.position = objectStorage;
+                rayCube.transform.position = centerCubePosition;
+                gazeCube.transform.position = objectStorage;
+                break;
+
+            case 3:
+                textMesH.text = "Finally, the gaze interaction. Focus the object on the center of your screen. It will light up whenever you look at it, but will not activate if your eyes are not tracking the object. " +
+                    "You can notice this in your peripheral vision.";
+                followMeRay.enabled = false;
+                rayCube.transform.position = objectStorage;
+                gazeCube.transform.position = centerCubePosition;
                 break;
         }
     }
